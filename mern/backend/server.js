6b26d1730989d13c6c4d9c37456163f7aae491c4
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-05-05 12:51:28 +0200

    initial rest api for goals

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv').config();
 const {errorHandler} =require('./middleware/errorMiddleware')
 const port = process.env.PORT ||5000;
 const app= express();
+const connectDB = require('./config/db')
+const colors =require('colors')
 
+connectDB()
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use('/api/goals',require('./routes/goalRoutes'));
